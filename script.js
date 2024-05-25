d97cbd26428dc984f03b130b4190a402f069a30c
joanna broad <joanna.broad93@gmail.com>
2024-05-25 11:39:08 +0100

    added in a function to generate a tagget number and a function for working out the differences between the human guess and the target

--- script.js
+++ script.js
@@ -3,4 +3,20 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+// function to randomly generate a number from 0-9
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+}
 
+// function to compare the guess as well as the computerGuess to the target
+const compareGuesses = (currentHumanGuess, computerGuess, target) => {
+  const humanDiff = Math.abs(currentHumanGuess - generateTarget);
+  const computerDiff = Math.abs(computerGuess - generateTarget);
+  if (humanDiff < computerDiff) {
+    return true;
+  } else if (humanDiff > computerDiff) {
+    return false;
+  } else {
+    return true;
+  }
+}
